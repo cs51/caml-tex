1a5ac13a081561d9d8e1c9e14ebda64f741c6274
Samuel Green <green.sam1192@gmail.com>
2016-10-16 12:45:42 -0400

    re-write using pexpect to manipute the ocaml shell

--- caml-tex.py
+++ caml-tex.py
@@ -21,10 +21,24 @@ def read_options():
 
 	return parser.parse_args()
 
-START_REGEX = r'\\begin{caml_(example|example\*|eval)}\s*'
-END_REGEX = r'\\end{caml_(example|example\*|eval)}\s*'
+def process_inline(line):
+	joined = '\\?{' + line + '}\n'
+	return joined
 
-def read_ml_block(filepointer):
+def process_outline(line):
+	return '\\:{' + line + '}\n'
+
+END_REGEX = "\\end{caml_(example|example\*|eval)}\s*"
+
+START_ECHO_EXAMPLE = r'\\begin{caml_example}\s*'
+
+START_NOECHO_EXAMPLE = r'\\begin{caml_example*}\s*'
+
+START_EVAL = r'\\begin{caml_eval*}\s*'
+
+def read_ml_block(filepointer, ocaml_session, echo_eval=True):
+	total_eval = ''
+
 	ml = ""
 
 	while True:
@@ -35,84 +49,113 @@ def read_ml_block(filepointer):
 			print "Opened ML expression never closed."
 			exit(1)
 
-		# if end of an ML embedding, move on
-		if re.search(END_REGEX, l):
-			return ml
+		# found the end of the block
+		elif re.search(END_REGEX, l):
+			break
+
+		# if an ocaml expression ends here
+		elif ";;" in l:
+			ml = ml + l
+
+			ocaml_session.sendline(ml)
+
+			ocaml_session.expect('#')
+
+			# get back what we just read and evaluated
+			evaled = ocaml_session.before
+
+			# getting a bit hacky here...
+			indx =  evaled.find(';;')
+			
+			inputted = evaled[:indx + 2]
+			outputted = evaled[indx + 2:]
+
+			clean_input = map(process_inline,
+				 	filter(lambda s : len(s) > 0, 
+						map(lambda s : s.strip(), inputted.split('\r\n'))))
+
+			clean_output = map(process_outline, 
+					filter(lambda s : len(s) > 0,
+						map( lambda s : s.strip(), outputted.split('\r\n'))))
+
+			if echo_eval:
+				total_eval += ''.join(clean_input) + ''.join(clean_output)
+			else:
+				total_eval += ''.join(clean_input)
+
+			ml = ""
+
 		# seeing more ML
+
 		else:
 			ml = ml + l
 
-OUTFILE = 'input.ml'
+	return total_eval
 
-def extract_ml(filename):
+import pexpect
+
+def convert_to_ml(filename, outfilename):
+
+	# start up and wait for the shell to be ready 
+	ocaml_session = pexpect.spawn('ocaml')
+	ocaml_session.expect("#")
+
+	# get the source file and the output file
 	try:
-		with open(filename, 'r') as infile:
-			while True:
-				l = infile.readline()
-
-				if not l:
-					break
-
-				if re.search(START_REGEX, l):
-					ml = read_ml_block(infile)
-
-					with open(OUTFILE, 'a') as outfile:
-						outfile.write(ml)
-
+		infile = open(filename, 'r')
 	except Exception as e:
-		print e
+		print "Input file error: {}".format(e)
 		exit(1)
-
-def eval_output():
-
-	with open(OUTFILE, 'r') as mlfile:
-		with open('evaled.txt', 'w') as outfile:
-			try:
-				
-				toplevel = subprocess.check_output('ocaml', stdin = mlfile) 
-
-				return toplevel
-
-			except subprocess.CalledProcessError as e:
-				print e
-				exit(2)
-
-def build_outfile(optional, infile):
-	if optional is "":
-		return infile + '.tex'
-
-			
-def swap_eval(infilename, outfilename, evaled_output):
-	try:
-		infile = open(infilename, 'r')
-	except:
-		print "Could not open infile."
-		exit(3)
-
+	
 	try:
 		outfile = open(outfilename, 'w')
-	except:
-		print "Could not open output .tex file."
-		exit(3)
+	except Exception as e:
+		print "Output file error: {}".format(e)
+		exit(1)
+
+	while True:
+
+		l = infile.readline()
+
+		# if we've hit end of line, get out of here
+		if not l:
+			infile.close()
+			outfile.close()
+			exit(0)
+
+		if re.search(START_ECHO_EXAMPLE, l):
+			evaled = read_ml_block(infile, ocaml_session)
+
+			outfile.write('\\begin{caml}\n')
+			outfile.write(evaled)
+			outfile.write('\\end{caml}\n')
+
+		elif re.search(START_NOECHO_EXAMPLE, l):
+			evaled = read_ml_block(infile, ocaml_session, add_eval = False)
+
+			outfile.write('\\begin{caml}\n')
+			outfile.write(evaled)
+			outfile.write('\\end{caml}\n')
+
+		# add the evaled block through the session
+		# but don't echo the output
+		elif re.search(START_EVAL, l):
+			read_ml_block(infile, ocaml_session)
+
+		# otherwise, this line is just .tex and should be echoed
+		else:
+			outfile.write(l)
 
 
 if __name__ == '__main__':
 	(options, args) = read_options()
 
-	try:
-		os.remove(OUTFILE)
-	except OSError:
-		pass
-
 	for arg in args:
 		
-		extract_ml(arg)
-		
-		output = eval_output()
+		if options.outfile is "":
+			options.outfile = arg + '.tex'	
 
-		outfile = build_outfile(options.outfile, arg)
-
-		swap_eval(arg, outfile, output)
+		convert_to_ml(arg, options.outfile)
 
 		break
 
