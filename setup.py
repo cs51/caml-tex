ca9d9a2ebd4cac9cc00038d5da6724d89e512363
Samuel Green <green.sam1192@gmail.com>
2017-04-23 17:17:20 -0400

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='camltex',
-        version='0.1.1',
+        version='0.1.2',
         description='CLI for processing embedded OCaml.',
         url='http://github.com/cs51/camltex',
         author='Sam Green',
